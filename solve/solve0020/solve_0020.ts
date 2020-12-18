aac677ae9ecd59d1e571a1317fbaf91bdbbe6e26
Yixuan Xu <yixuanxu94@gmail.com>
2020-12-18 10:44:40 +0800

    update

--- solve/solve0020/solve_0020.ts
+++ solve/solve0020/solve_0020.ts
@@ -18,13 +18,17 @@ export function isValid(s: string): boolean {
   }
   for (let i = 0; i < strArr.length; i += 1) {
     if (strArr[i] === '(' || strArr[i] === '[' || strArr[i] === '{') {
+      // 如果是左边括号 那么往栈中添加一个反向括号
       stack.push(pair[strArr[i]])
     } else {
+      // 如果是右边括号 那么检查栈顶元素 如果栈顶元素不同 则不是有效的括号
       if (stack.length === 0 || stack[stack.length - 1] !== strArr[i]) {
         return false
       }
+      // 相同则将该括号从栈中移除
       stack.pop()
     }
   }
+  // 如果栈中在最后为空 则说明是有效的括号
   return stack.length === 0
 }
--- solve/solve0026/solve_0026.test.ts
+++ solve/solve0026/solve_0026.test.ts
@@ -1,3 +1,7 @@
+import { removeDuplicates } from './solve_0026'
 it('solve_26 should pass', () => {
   const a = [1, 1, 2]
+  const result = removeDuplicates(a)
+  expect(result).toBe(2)
+  expect(a).toEqual([1, 2, 2])
 })
